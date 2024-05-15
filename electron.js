e60a341e01ed441f09bfcce872d1f8182fe1680e
ZerubabelY <zerubabel.kassahun@aastustudent.edu.et>
2024-05-15 10:17:06 +0300

    Components and implementation

---
+++ electron.js
@@ -0,0 +1,18 @@
+// electron.js
+
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+
+    width: 1000,
+    height: 800,
+  });
+
+  win.loadURL("http://localhost:3000"); // Load React app
+}
+
+app.whenReady().then(createWindow);
